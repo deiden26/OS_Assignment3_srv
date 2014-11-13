b5e4346df70d031a9f4f8dc40aa58cbb35fc1b57
Chip Weinberger <weinberger.c@gmail.com>
2014-11-13 06:58:00 -0600

    initial untested semaphore implementation usinglocks

--- semaphore.c
+++ semaphore.c
@@ -5,19 +5,29 @@
 
 typedef struct m_sem_t {
     int value;
+    pthread_mutex_t lock;
 } m_sem_t;
 
-int sem_wait(m_sem_t *s);
-int sem_post(m_sem_t *s);
 
-int sem_wait(m_sem_t *s)
-{
-    //TODO
-    return 0;
+int sem_wait(m_sem_t *s){
+	
+	while(*s->value == 0){}
+
+	pthread_mutex_lock(&(*s->lock));
+
+	*s->value = *s->value-1;
+
+	pthread_mutex_unlock(&(*s->lock))
+
 }
 
-int sem_post(m_sem_t *s)
-{
-    //TODO
-    return 0;
+
+int sem_post(m_sem_t *s){
+	pthread_mutex_lock(&(*s->lock));
+
+	*s->value = *s->value+1;
+
+	pthread_mutex_unlock(&(*s->lock))
+
 }
+
