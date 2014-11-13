e697bf7816093c79d18659d7a9c7754aacc1367e
Chip Weinberger <weinberger.c@gmail.com>
2014-11-13 07:09:57 -0600

    fixed compiler errors in semaphore

--- semaphore.c
+++ semaphore.c
@@ -11,23 +11,23 @@ typedef struct m_sem_t {
 
 int sem_wait(m_sem_t *s){
 	
-	while(*s->value == 0){}
+	while(s->value == 0){;}
 
-	pthread_mutex_lock(&(*s->lock));
+	pthread_mutex_lock(&(s->lock));
 
-	*s->value = *s->value-1;
+	s->value = s->value-1;
 
-	pthread_mutex_unlock(&(*s->lock))
+	pthread_mutex_unlock(&(s->lock));
 
 }
 
 
 int sem_post(m_sem_t *s){
-	pthread_mutex_lock(&(*s->lock));
+	pthread_mutex_lock(&(s->lock));
 
-	*s->value = *s->value+1;
+	s->value = s->value+1;
 
-	pthread_mutex_unlock(&(*s->lock))
+	pthread_mutex_unlock(&(s->lock));
 
 }
 
